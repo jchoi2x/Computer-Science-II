283a825d9ce31019b0320b99e34260553f678b65
James Choi <jchoi2012@knights.ucf.edu>
2014-02-03 22:28:26 +0000

    almost done

---
+++ assignment/agn2/Empirical.java
@@ -0,0 +1,126 @@
+import java.io.*;
+import java.util.*;
+
+public class Empirical {
+
+	public static int mcss_cubed(int [] array) {
+		int max = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i; j < array.length; j++) {
+				int sum = 0;
+
+				for (int k = i; k <= j; k++)
+					sum += array[k];
+
+				if (sum > max)
+					max = sum;
+			}
+		}
+		return max;
+	}
+
+	public static int mcss_squared(int [] array) {
+		int max = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			int sum = 0;
+			for (int j = i; j < array.length; j++) {
+				sum += array[j];
+
+				if (sum > max)
+					max = sum;
+			}
+		}
+		return max;
+	}
+
+	public static int mcss_linear(int [] array) {
+		int max = 0, sum = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+
+			if (sum > max)
+				max = sum;
+			else if (sum < 0)
+				sum = 0;
+		}
+		return max;
+	}
+	
+
+	public static void readIn(String filename){
+        
+        
+	    try{
+	        Scanner scan = new Scanner(new FileReader(filename)); // Read from input stream
+	        // read the first line containing number of testcases
+	        int numCases = Integer.parseInt(scan.nextLine().split("\\s+")[0]);
+	        int[] array ; 
+	        for ( int j = 0  ; j < numCases ; j++ ){
+	            // make temp string array and read into it
+                String[] testCase = scan.nextLine().split("\\s+");
+                // Init int array
+                array = new int[testCase.length-1];
+                for ( int i = 1 ; i < testCase.length ; i++ ){
+                    array[i-1] = Integer.parseInt(testCase[i]);
+                }
+                runTest(array,50);
+	        }
+        
+            
+	    }catch(IOException ex){
+            System.out.println("ERROR IOException");
+	    }
+	     
+	}
+	
+	public static void runTest(int[] array, int numTimes){
+	    double[] avg = new double[3];
+	    long start; 
+	    long end ; 
+	    int sol = 0; 
+        //----------------------------------------------------------
+	    start = System.nanoTime();
+	    for ( int i = 0 ; i < numTimes ; i++ ){
+	        mcss_cubed(array);
+	    }
+	    end = System.nanoTime();
+        avg[0] = (((end - start) / 1000000.0) / numTimes)*1000000.0 ;
+	    
+        //----------------------------------------------------------
+	    start = System.nanoTime();
+	    for ( int i = 0 ; i < numTimes ; i++ ){
+	        mcss_squared(array);
+	    }
+	    end = System.nanoTime();
+        avg[1] = (((end - start) / 1000000.0) / numTimes)*1000000.0 ;
+        
+        //----------------------------------------------------------
+	    start = System.nanoTime();
+	    for ( int i = 0 ; i < numTimes ; i++ ){
+	        sol = mcss_linear(array);
+	    }
+	    end = System.nanoTime();
+        avg[2] = (((end - start) / 1000000.0) / numTimes)*1000000.0 ;
+        
+        System.out.println("\n"+sol+" "+String.format("%.0f",avg[0])+" "+String.format("%.0f",avg[1])+" "+String.format("%.0f",avg[2]));
+	}
+	public static void main(String [] args) {
+
+
+
+		// create and populate array with random ints on range [-1000, 1000]
+        //int[] array = getRandArray(N,1000);
+		readIn("infile.txt");
+		
+		// start timer
+
+        
+
+		// convert nanoseconds to ms and divide by numRuns to get average runtime
+		// for a single function call
+	//	System.out.println("Runtime: " + (((end - start) / 1000000.0) / numRuns) + "ms");
+	}
+}
--- assignment/agn2/EmpiricalAnalysis.java
+++ assignment/agn2/EmpiricalAnalysis.java
@@ -1,3 +1,12 @@
+// Sean Szumlanski
+// COP 3503, Fall 2013
+
+// EmpiricalAnalysis.java
+// ======================
+// This program contains the three implementations of the MCSS algorithm that
+// we saw in class on Monday, August 26, 2013. It provides a framework for
+// timing how long each algorithm takes to execute for different input sizes, to
+// facilitate empirical analysis of Big-Oh runtime.
 
 
 import java.io.*;
@@ -50,49 +59,30 @@ public class EmpiricalAnalysis {
 		}
 		return max;
 	}
-	
-    public static int[] getRandArray(int size, int range){
-        int[] array = new int[size];
-        for ( int i = 0 ; i < size ; i++ ){
-            array[i] = (int)(Math.random()*(range*2+1))-range;
-        }
-        return array ; 
-    }
-	public static void readIn(String filename){
-	    try{
-            Scanner scan = new Scanner(new FileReader(filename)); // Read from input stream
-            String[] testCase = scan.nextLine().split("\\s+");
-            for ( int i = 0 ;i < testCase.length ; i++ ){
-                System.out.print(" "+testCase[i]);
-            }
-            
-            //return testCase ; 
-	    }catch(IOException ex){
-            System.out.println("ERROR IOException");
-	    }
-	}
-	
+
 	public static void main(String [] args) {
 
-		//int N = 1000;
-		//int numRuns = 50;
+		int N = 1000;
+		int numRuns = 50;
 
 		// create and populate array with random ints on range [-1000, 1000]
-        //int[] array = getRandArray(N,1000);
-		readIn("infile.txt");
+		int [] array = new int[N];
+
+		for (int i = 0; i < N; i++)
+			array[i] = (int)(Math.random() * 2001) - 1000;
+
 		// start timer
 		final long start = System.nanoTime();
 
-        
 		// run function: mcss_cubed(array), mcss_squared(array), or mcss_linear(array)
-		//for (int i = 0; i < numRuns; i++)
-		//	mcss_squared(array);
+		for (int i = 0; i < numRuns; i++)
+			mcss_squared(array);
 
 		// end timer
-	//	final long end = System.nanoTime();
+		final long end = System.nanoTime();
 
 		// convert nanoseconds to ms and divide by numRuns to get average runtime
 		// for a single function call
-	//	System.out.println("Runtime: " + (((end - start) / 1000000.0) / numRuns) + "ms");
+		System.out.println("Runtime: " + (((end - start) / 1000000.0) / numRuns) + "ms");
 	}
 }
