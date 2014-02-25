227cb2eb71bcbe1e94f689109ef08a99fb440580
kizzlebot <jchoi2012@knights.ucf.edu>
2014-02-25 17:58:24 -0500

    refine

--- assignment/agn3/TopoSort.java
+++ assignment/agn3/TopoSort.java
@@ -1,7 +1,9 @@
-/* Name:    James Choi
- * Course:  Computer Science II
- * Assignment #3 - Topological Sort  
- * Implement Topological sort via DepthFirst Search and Decrease by one */
+/*        Name:  James Choi
+ *      Course:  Computer Science II COP3503C
+ *     Section:  14
+ *  Assignment:  Topological Sort
+ * Description:  Implementation of Topological sorting algorithms
+ *               DepthFirst Search and Decrease-By-One               */
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,16 +26,16 @@ public class TopoSort {
 
 
 	/**
-	 * Initializes utilities and calls the recursive dfs then prints the 
+	 * Initializes utilities and calls the recursive dfs then prints the
 	 * results of topological sort when dfs returns.
 	 * @param matrix
 	 */
 	public void dfsCaller(Integer[][] matrix) {
 		stk.clear();
 		visited.clear();
-		
+
 		for ( int i = 0 ; i < matrix.length ; i++ )visited.add(false);
-		
+
 		// While unvisited vertices exist
 		while ( visited.indexOf(false) != -1 ){
 			for ( int i = 0 ; i < visited.size() ; i++ ){
@@ -48,23 +50,23 @@ public class TopoSort {
 		}
 	}
 	/**
-	 * Recursive Depthfirst search. Pushes to a stack only so 
+	 * Recursive Depthfirst search. Pushes to a stack only so
 	 * dfsCaller can print in reverse order when dfs returns
 	 * @param matrix
-	 * @param v 				
+	 * @param v
 	 **/
 	public void dfs (Integer[][] matrix, Integer v){
 		// mark v visited
 		visited.set(v, true);
 
-		// for each vertex w pointed to by v  
+		// for each vertex w pointed to by v
 		for ( Integer w : getOutgoing(matrix, v) ) {
 			// if w is unvisited then dfs(w)
 			if (!visited.get(w)) {
 				dfs(matrix,w);
 			}
 		}
-		
+
 		// push(v) into stack so it can be printed in reverse l8r
 		stk.push(v);
 	}
@@ -79,48 +81,48 @@ public class TopoSort {
         ArrayList<Integer> L = new ArrayList<Integer>();
         // S <- Set of all nodes with no incoming edges
         ArrayList<Integer> S = getNoIncoming(matrix);	// Fill S w. vertices w/o incoming edges
-       
+
         // while S is nonempty
         while (!S.isEmpty()){
             // remove a node n from S and insert into L
         	Integer n = S.remove(0);
         	L.add(n);
-        	
+
             // for each node m with an edge from n to m
         	for ( Integer m : getOutgoing(matrix, n)){
         		// remove edge e from the graph
         		matrix[n][m] = 0 ;
-        		
+
 
                 // if m has no other incoming edges
         		if (getNoIncoming(matrix, m)){
         			// then insert m in S
         			S.add(m);
         		}
-        	}            
+        	}
         }
 	    // if graph has edges then ERROR
         for ( int i = 0 ; i < matrix.length ; i++ ){
         	if ( getOutgoing(matrix,i).size() != 0 ){
         		System.out.println("\nERROR");
-        		return ; 
+        		return ;
         	}
         }
-        
+
         // Otherwise print L
     	for ( Integer n : L ){
     		System.out.printf("%d ", n);
     	}
     }
-	
-	
-	
 
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
 	/**
 	 * From input file, reads the number matrixes and for each matrix read,
 	 * performs depth-first search and decrease-by-one search on the matrix
@@ -143,7 +145,7 @@ public class TopoSort {
 				// the adjacency matrix values
 				Integer[][] matrix = new Integer[n_by_n][n_by_n];
 
-				// for each next line 
+				// for each next line
 				for (int j = 0; j < n_by_n && scan.hasNextLine(); j++) {
 					// Read the line as one long string, and split at whitespace
 					// and split into an array before assigning to aLine
@@ -154,10 +156,10 @@ public class TopoSort {
 						matrix[j][k] = Integer.parseInt(aLine[k]);
 					}
 				}
-				
-				
-				
-				
+
+
+
+
 				/*
 				 * Execute Depth-First Search and Decrease-by-One methods
 				 */
@@ -173,7 +175,7 @@ public class TopoSort {
 					System.out.printf("TS("+i+",DFS): NO TOPOLOGICAL SORT");
 					System.out.printf("\nTS("+i+",DBO): NO TOPOLOGICAL SORT");
 					System.out.println();
-					
+
 				}
 			}
 			scan.close();
@@ -185,8 +187,8 @@ public class TopoSort {
 	}
 
 	/**
-	 * Returns arraylist containing vertices with no incoming edges 
-	 * of the entire matrix. Vertex v has no incoming edges if 
+	 * Returns arraylist containing vertices with no incoming edges
+	 * of the entire matrix. Vertex v has no incoming edges if
 	 * column v of adj. matrix is all 0's
 	 * @param matrix
 	 * @return ArrayList containing vertices with no incoming edges
@@ -197,10 +199,10 @@ public class TopoSort {
         for ( int i = 0 ; i < matrix.length ; i++ ){
         	// for each row in column
             for ( int j = 0 ; j < matrix[i].length ; j++){
-            	
+
                 if ( matrix[j][i] == 1 )
-                	break; 
-                
+                	break;
+
                 // If at final row j of column i, and matrix contains a 0 add it
                 if ( j+1 >= matrix[i].length && matrix[j][i] == 0){
                     rtn.add(i);
@@ -209,7 +211,7 @@ public class TopoSort {
         }
         return rtn ;
     }
-    
+
     /**
      * (Overloaded method) Returns true if vertex m has no incoming edges. Otherwise false
      * @param matrix
@@ -219,40 +221,40 @@ public class TopoSort {
 	public Boolean getNoIncoming(Integer[][] matrix, Integer m){
 		for ( int i = 0 ; i < matrix.length ; i++){
 			if (matrix[i][m] == 1){
-				return false ; 
+				return false ;
 			}
 		}
-		return true ; 
+		return true ;
 	}
-	
+
     /**
-     * Finds and returns Arraylist containing all the vertices that m points. 
+     * Finds and returns Arraylist containing all the vertices that m points.
      * @param matrix
      * @param m
-     * @return Arraylist containing vertices that m points to 
+     * @return Arraylist containing vertices that m points to
      */
 	public ArrayList<Integer> getOutgoing(Integer[][] matrix, Integer m){
 		ArrayList<Integer> rtn = new ArrayList<Integer>();
-		
-		// Get the first instance of a 1 in the row of m 
+
+		// Get the first instance of a 1 in the row of m
 		int index = Arrays.asList(matrix[m]).indexOf(1);
 		// If no 1 then return rtn
-		if ( index == -1) return rtn ; 
-		
+		if ( index == -1) return rtn ;
+
 		for ( int i = index ; i < matrix.length ; i++ ){
             if ( matrix[m][i] == 1 ){
             	rtn.add(i);
             }
         }
-		return rtn ; 
+		return rtn ;
 	}
-	
+
 	/**
-	 * Tests for acyclicity of matrix by removing a leaf node 
-	 * from given matrix and recursing until either the matrix is complely 
-	 * empty (acyclic) or there are no leaves in the matrix (cyclic) 
+	 * Tests for acyclicity of matrix by removing a leaf node
+	 * from given matrix and recursing until either the matrix is complely
+	 * empty (acyclic) or there are no leaves in the matrix (cyclic)
 	 * @param matrix
-	 * @return True if matrix is acyclic and false otherwise 
+	 * @return True if matrix is acyclic and false otherwise
 	 */
 	public Boolean acyclicTest(Integer[][] matrix) {
 		if (matrix.length == 0) {
@@ -266,9 +268,9 @@ public class TopoSort {
 			return acyclicTest(removeVertex(matrix, findLeaf(matrix) ));
 		}
 	}
-	
+
 	/**
-	 * Removes the target row and target column from the given matrix and returns it 
+	 * Removes the target row and target column from the given matrix and returns it
 	 * @param matrix
 	 * @param target
 	 * @return Modified version of argument matrix, with row target and column target removed
@@ -281,7 +283,7 @@ public class TopoSort {
 		}
 		int row = 0, col = 0;
 		for (int i = 0; i < matrix.length; i++) {
-			if (i == target) continue;	
+			if (i == target) continue;
 			else{
 				for ( int j = 0 ; j < matrix[i].length ; j++ ){
 					if ( j == target ) continue ;
