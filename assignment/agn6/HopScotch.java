4844f9ec7a3646556b6cfe45edee1daf35350fb1
kizzlebot <jchoi2012@knights.ucf.edu>
2014-04-16 19:56:47 -0400

    submission

--- assignment/agn6/HopScotch.java
+++ assignment/agn6/HopScotch.java
@@ -10,30 +10,28 @@ import java.util.Arrays;
 
 
 public class HopScotch {
-    private int[] F;
+    private int[] F;                                    // Let F(n) be the minimum score possible starting at n
     public HopScotch(String fileName) {
-        this.F = new int[7];
-        for ( int i = 0 ; i < 7 ; i++ ) this.F[i] = i ;
-        readIn(fileName);
+        this.F = new int[7];                            // Initial size of F array
+        for ( int i = 0 ; i < 7 ; i++ ) this.F[i] = i ; // Fill with base cases
+        readIn(fileName);                               // Read in the input file
     }
 
     /**
      * Finds minimum score possible to reach end of row
      * @param n Starting box number
-     * @return
+     * @return Minimum score possible to reach box 0
      */
     private int hop(int n) {
+        if ( n < F.length ) return F[n] ;     // If n is a value aready calculated, then return it
+        int l = F.length;                     // Save length of F before resizing
+        int[] rtn = new int[n+1];             // Make new array to resize F to
+        Arrays.fill(rtn,0);                   // Initially fill with 0's
+        System.arraycopy(F,0,rtn,0,F.length); // Copy all the old values of F into new Array
+        this.F = rtn ;                        // Make F refer to the resize Array
 
-        if ( n < F.length ) return F[n] ;
-
-        int l = 0;
-        int[] rtn = new int[n+1];
-        l = F.length-1 ;
-        Arrays.fill(rtn,0);
-        System.arraycopy(F,0,rtn,0,F.length);
-        this.F = rtn ;
-
-        for ( int i = l+1 ; i <= n ; i++ ){
+        for ( int i = l ; i <= n ; i++ ){
+            // Determine if any of special cases apply
             boolean prime = isPrime(i);
             boolean factor11 = (i%11 == 0);
             boolean factor7 = (i%7 == 0);
@@ -59,15 +57,15 @@ public class HopScotch {
     }
 
     /**
-     * Given a value, will compute the sum of the digits that compose the value
+     * Given a value, will compute the sum of the digits of the given value
+     * ie n = 11 , then will return 2
      * @param a
-     * @return  Sum of digits of argument 'a'
-     */
-    private int digitSum(int a){
+     * @return  Sum of digits of argument 'a'           */
+    private int digitSum(int n){
         int sum = 0;
-        while (a != 0){
-            sum += a%10;
-            a /= 10 ;
+        while (n != 0){
+            sum += n%10;
+            n /= 10 ;
         }
         return sum ;
     }
@@ -76,10 +74,14 @@ public class HopScotch {
      * @param a
      * @return True if a is prime
      */
-    private boolean isPrime(int a) {
-        if (a < 10 || a % 2 == 0) return false;
-        for ( int i = (int) Math.ceil((Math.sqrt(a))) ; i > 1; i--)
-            if (a % i == 0) return false;
+    private boolean isPrime(int n) {
+        // If n is less than 10 or divisible by two, return false immediately
+        if (n < 10 || n % 2 == 0) return false;
+        /* Starting from the largest i that can possibly be a factor of n
+         * if a factor is found, return false       */
+        for ( int i = (int) Math.ceil((Math.sqrt(n))) ; i > 1; i--)
+            if (n % i == 0) return false;
+        // Must be prime
         return true;
     }
 
@@ -101,6 +103,6 @@ public class HopScotch {
     }
 
     public static void main(String[] args) {
-        new HopScotch("infile.txt");
+        new HopScotch("hopscotch.in");
     }
 }
